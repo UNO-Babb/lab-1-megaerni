d7b543c6c1915c2578f284f1ab26b33ad5049f37
megaerni <165853906+megaerni@users.noreply.github.com>
2025-08-28 20:15:08 +0000

    Done with lab!

--- FirstProgram.py
+++ FirstProgram.py
@@ -6,11 +6,10 @@
 def main():
   print("First Program")
   #Say hello
-  
+
   #Ask for the user's name
 
   #Use the user's name in the program.
-
   #Ask the user for their age.
 
   #Tell the user what year they were born in.
--- MadLib.py
+++ MadLib.py
@@ -1,13 +1,22 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Meg Aerni
+#Date: 8/28/2025
+#Assignment: Lab #1
 
 def main():
   print("Madlib")
   #Ask user for words
-
+var1 = input("Name an adjective: ")
+var2 = input("Name a verb: ")
+var3 = input("Name an animal: ")
+var4 = input("Give me a number: ")
+var5 = input("Name a place: ")
+var6 = input("Name an adjective: ")
   #Print the story with the user supplied words.
+print("Once upon a time, there was a " + var1 + " girl who loved to " + var2 + ". One day, while she was " + var2 + "ing, a "
++ var3 + " appeared out of nowhere! The girl shrieked and jumped " + var4 + " feet off the ground. 'Where did you come from?', asked the girl. 'From " + 
+var5 + "', the " + var3 + " replied. Despite the initial scare, the girl realized that the " + var3 + 
+" was very " + var6 + ", so they became friends and lived happily ever after.")
 
 
 
